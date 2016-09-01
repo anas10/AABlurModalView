0a1ef655b88fd8910fb90b38d264237d90a07ad4
Anas AIT ALI <aitali.anas@gmail.com>
2016-09-01 15:03:26 +0100

    Add hideBlurModalView with identifier

--- Source/AABlurModalView.swift
+++ Source/AABlurModalView.swift
@@ -16,6 +16,7 @@ public class AABlurModalView: UIView {
 
     public var contentSize : CGSize?
     public var blurEffectStyle: UIBlurEffectStyle = .Dark
+    public var identifier: String?
 
     private var contentView : UIView!
     private var backgroundImage : UIImageView!
@@ -42,31 +43,6 @@ public class AABlurModalView: UIView {
         commonInit(UIView(), contentSize: nil)
     }
 
-    private func commonInit(contentView: UIView, contentSize: CGSize? = nil) {
-        self.opaque = true
-        self.alpha = 1
-        self.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        self.backgroundImage = UIImageView(frame: self.bounds)
-        self.backgroundImage.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        self.addSubview(backgroundImage)
-
-        self.contentView = contentView
-        self.contentSize = contentSize
-    }
-
-    private func setupContentView() {
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(contentView)
-        let viewDict = ["cV":contentView, "view":self]
-        let cSize = contentSize ?? contentView.frame.size
-        let metrics = ["cVHeight": cSize.height, "cVWidth": cSize.width]
-        [NSLayoutConstraint.constraintsWithVisualFormat("V:[cV(cVHeight)]", options: [], metrics: metrics, views: viewDict),
-            NSLayoutConstraint.constraintsWithVisualFormat("H:[cV(cVWidth)]", options: [], metrics: metrics, views: viewDict),
-            NSLayoutConstraint.constraintsWithVisualFormat("H:[view]-(<=1)-[cV]", options: NSLayoutFormatOptions.AlignAllCenterY, metrics: nil, views: viewDict),
-            NSLayoutConstraint.constraintsWithVisualFormat("V:[view]-(<=1)-[cV]", options: NSLayoutFormatOptions.AlignAllCenterX, metrics: nil, views: viewDict)
-            ].forEach { NSLayoutConstraint.activateConstraints($0) }
-    }
-
     public func show() {
         setupContentView()
         // TODO : Add alternative solution if the snapshot fails
@@ -90,6 +66,41 @@ public class AABlurModalView: UIView {
     public func hide() {
         self.backgroundImage.subviews.forEach { $0.removeFromSuperview() }
         self.removeFromSuperview()
+    }
+
+    public static func hideBlurModalView(identifier: String?) {
+        if let identifier = identifier {
+            UIApplication.sharedApplication().delegate?.window??.subviews
+                .filter { $0.isKindOfClass(AABlurModalView) }
+                .filter { ($0 as? AABlurModalView)?.identifier == identifier }
+                .forEach { $0.hideBlurModalView() }
+        }
+    }
+
+    private func commonInit(contentView: UIView, contentSize: CGSize? = nil) {
+        self.opaque = true
+        self.alpha = 1
+        self.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.backgroundImage = UIImageView(frame: self.bounds)
+        self.backgroundImage.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(backgroundImage)
+
+        self.contentView = contentView
+        self.identifier = contentView.accessibilityIdentifier
+        self.contentSize = contentSize
+    }
+
+    private func setupContentView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(contentView)
+        let viewDict = ["cV":contentView, "view":self]
+        let cSize = contentSize ?? contentView.frame.size
+        let metrics = ["cVHeight": cSize.height, "cVWidth": cSize.width]
+        [NSLayoutConstraint.constraintsWithVisualFormat("V:[cV(cVHeight)]", options: [], metrics: metrics, views: viewDict),
+            NSLayoutConstraint.constraintsWithVisualFormat("H:[cV(cVWidth)]", options: [], metrics: metrics, views: viewDict),
+            NSLayoutConstraint.constraintsWithVisualFormat("H:[view]-(<=1)-[cV]", options: NSLayoutFormatOptions.AlignAllCenterY, metrics: nil, views: viewDict),
+            NSLayoutConstraint.constraintsWithVisualFormat("V:[view]-(<=1)-[cV]", options: NSLayoutFormatOptions.AlignAllCenterX, metrics: nil, views: viewDict)
+            ].forEach { NSLayoutConstraint.activateConstraints($0) }
     }
 
     private func snapshot() -> UIImage? {
